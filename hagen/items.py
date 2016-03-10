48dd9b9e57b6dd128125ec53b2752783e3a7aa15
Danil Bokov <a35012a@gmail.com>
2016-03-10 17:34:44 +0600

    Deleted scrapy comments

--- hagen/items.py
+++ hagen/items.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
-
 
 class HagenItem(scrapy.Item):
 	title = scrapy.Field()
