4c0ce6a63d46c271b7c381728ac3da00179b770a
Danil Bokov <a35012a@gmail.com>
2016-03-10 17:38:29 +0600

    Changed settings

--- hagen/settings.py
+++ hagen/settings.py
@@ -14,17 +14,16 @@ BOT_NAME = 'hagen'
 SPIDER_MODULES = ['hagen.spiders']
 NEWSPIDER_MODULE = 'hagen.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS=32
+CONCURRENT_REQUESTS=4
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
+DOWNLOAD_DELAY=3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
